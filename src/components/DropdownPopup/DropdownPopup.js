0315868a31a0d690a600508765d878861f1db095
Humaz-asoft <90444126+HumazMiraj@users.noreply.github.com>
2021-09-28 18:52:48 +0500

    dropdown

--- src/components/DropdownPopup/DropdownPopup.js
+++ src/components/DropdownPopup/DropdownPopup.js
@@ -1,21 +1,25 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 
-function Dropdown(){
-    return(
-        <div className="dropdown-popup">
-            <div className="dropdown-content">
-                <div className="right-icon float-end mt-2">
-                        <img className="img-fluid" src="images/user-icon.svg" />
+function Dropdown(props) {
+    return (
+        <>
+            {props.isopen ?
+                <div className="dropdown-popup">
+                    <div className="dropdown-content">
+                        <div className="right-icon float-end mt-2" >
+                            <img className="img-fluid" src="images/user-icon.svg" />
+                        </div>
+                        <div className="cross-btn float-end" onClick={() => props.isopenhide()}>
+                            <FontAwesomeIcon icon={faTimes} />
+                        </div>
+                        <h2 className="sub-heading">Marv’hELL</h2>
+                    </div>
                 </div>
-                <div className="cross-btn float-end" onClick={() => props.hideModel()}>
-                    <FontAwesomeIcon icon={faTimes} />
-                </div>
-                <h2 className="sub-heading">Marv’hELL</h2>
-            </div>
-        </div>
+                : ''}
+        </>
     )
 
 }

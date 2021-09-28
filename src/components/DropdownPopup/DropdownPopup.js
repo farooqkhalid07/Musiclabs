3d91a7e32a0e3a4deef06b178a60da65022af82d
Farooq Khalid <farooq.khalid@arhamsoft.com>
2021-09-28 18:55:13 +0500

    fdgdf

--- src/components/DropdownPopup/DropdownPopup.js
+++ src/components/DropdownPopup/DropdownPopup.js
@@ -8,12 +8,22 @@ function Dropdown(){
         <div className="dropdown-popup">
             <div className="dropdown-content">
                 <div className="right-icon float-end mt-2">
-                        <img className="img-fluid" src="images/user-icon.svg" />
+                    <img className="img-fluid" src="images/user-icon.svg" />
                 </div>
                 <div className="cross-btn float-end" onClick={() => props.hideModel()}>
                     <FontAwesomeIcon icon={faTimes} />
                 </div>
                 <h2 className="sub-heading">Marv’hELL</h2>
+                <h3>Wallet account</h3>
+                <h5>0,00034</h5>
+                <span>3e1aaa2d5a4512e7ef8a011d8d4095800a4e6b3b0a0435355086630164586149</span>
+                <hr/>
+                <div className="d-flex">
+                    <div className="">
+
+                    </div>
+                </div>
+
             </div>
         </div>
     )
